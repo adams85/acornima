11194fee16519df925c74d7580bd8a49334505ee
Adam Simon <adamosimoni@gmail.com>
2024-11-16 20:38:33 +0100

    One more minor correction

--- samples/JsxTranspiler/generate.cmd
+++ samples/JsxTranspiler/generate.cmd
@@ -7,7 +7,7 @@ IF [%TARGET_FRAMEWORK%] == [] (
 
 REM Transpile template
 dotnet build -f %TARGET_FRAMEWORK%
-bin\debug\%TARGET_FRAMEWORK%\jsxt --type module < template.jsx > template.mjs
+%~dp0\bin\debug\%TARGET_FRAMEWORK%\jsxt --type module < template.jsx > template.mjs
 
 REM Generate output from template
 node generate.mjs
