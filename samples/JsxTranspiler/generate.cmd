a11e55d4c34507c1c1c603178c27cd0bd9c8183e
Adam Simon <adamosimoni@gmail.com>
2024-11-16 20:20:25 +0100

    Minor corrections

--- samples/JsxTranspiler/generate.cmd
+++ samples/JsxTranspiler/generate.cmd
@@ -1,7 +1,13 @@
 @echo off
 
+SET TARGET_FRAMEWORK=%1
+IF [%TARGET_FRAMEWORK%] == [] (
+  SET TARGET_FRAMEWORK=net6.0
+)
+
 REM Transpile template
-dotnet run -f net6.0 -- --type module < template.jsx > template.mjs
+dotnet build -f %TARGET_FRAMEWORK%
+bin\debug\%TARGET_FRAMEWORK%\jsxt --type module < template.jsx > template.mjs
 
 REM Generate output from template
 node generate.mjs
